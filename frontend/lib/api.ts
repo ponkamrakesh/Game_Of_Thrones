42f8fa331d14269f5d61822104f9072b4e155489
Rakesh <111850784+ponkamrakesh@users.noreply.github.com>
2026-03-26 00:07:46 +0530

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000"; // change after deploy
+const API_URL = "https://game-of-thrones-ngda3gxpw-ponkamrakesh-gmailcoms-projects.vercel.app"; // change after deploy
 
 export const askQuestion = async (question: string) => {
   const res = await axios.post(`${API_URL}/query`, { question });
