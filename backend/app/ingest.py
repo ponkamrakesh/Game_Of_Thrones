3047de528fc7ca82cdcb12690dae32b2e18900e7
Rakesh <111850784+ponkamrakesh@users.noreply.github.com>
2026-03-26 00:14:17 +0530

    Update ingest.py

--- backend/app/ingest.py
+++ backend/app/ingest.py
@@ -1,29 +1,50 @@
-from langchain_community.document_loaders import PyPDFLoader
+import os
+
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    TextLoader
+)
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-DATA_PATH = os.path.join(BASE_DIR, "data", "got_book.pdf")
+DATA_DIR = os.path.join(BASE_DIR, "data")
 VECTOR_PATH = os.path.join(BASE_DIR, "vectorstore")
 
-loader = PyPDFLoader(DATA_PATH)
-docs = loader.load()
+all_docs = []
 
+# 🔥 Load all files dynamically
+for file in os.listdir(DATA_DIR):
+    path = os.path.join(DATA_DIR, file)
+
+    if file.endswith(".pdf"):
+        loader = PyPDFLoader(path)
+        docs = loader.load()
+        all_docs.extend(docs)
+
+    elif file.endswith(".txt"):
+        loader = TextLoader(path)
+        docs = loader.load()
+        all_docs.extend(docs)
+
+print(f"Loaded {len(all_docs)} documents")
+
+# Split
 splitter = RecursiveCharacterTextSplitter(
     chunk_size=500,
     chunk_overlap=50
 )
 
-chunks = splitter.split_documents(docs)
+chunks = splitter.split_documents(all_docs)
 
+# Embeddings
 embeddings = HuggingFaceEmbeddings(
     model_name="BAAI/bge-small-en"
 )
 
+# Store
 db = FAISS.from_documents(chunks, embeddings)
 db.save_local(VECTOR_PATH)
 
-print("Vector DB created successfully.")
+print("Vector DB created from all files.")
